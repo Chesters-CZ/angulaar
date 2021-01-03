46e8911f0e823851c81046c57b7c29938b2db8f0
Chesters-CZ <bezouskavit@gmail.com>
2021-01-03 15:00:42 +0100

    edity

--- apliklace/src/app/app.module.ts
+++ apliklace/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ContactComponent } from './contact/contact.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ContactComponent} from './contact/contact.component';
 import {FormsModule} from "@angular/forms";
-import { StranadvjeComponent } from './stranadvje/stranadvje.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { HomeComponent } from './home/home.component';
-import { AboutcompComponent } from './aboutcomp/aboutcomp.component';
-import { HomecompComponent } from './homecomp/homecomp.component';
-import { BlogComponent } from './blog/blog.component';
+import {StranadvjeComponent} from './stranadvje/stranadvje.component';
+import {AboutusComponent} from './aboutus/aboutus.component';
+import {HomeComponent} from './home/home.component';
+import {AboutcompComponent} from './aboutcomp/aboutcomp.component';
+import {HomecompComponent} from './homecomp/homecomp.component';
+import {BlogComponent} from './blog/blog.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +23,13 @@ import { BlogComponent } from './blog/blog.component';
     HomecompComponent,
     BlogComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- apliklace/src/app/blog/blog.component.ts
+++ apliklace/src/app/blog/blog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-blog',
@@ -6,23 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
-  headerinput;
-  bodyinput;
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
 
   }
 
-  postBlog(header, body){
-    let outputPlace = document.getElementById("BlogPostY");
-    let obal = document.createElement("div");
-    let headrelem = document.createElement("h3");
-    headrelem.innerText = header;
-    obal.appendChild(headrelem);
-    let bodyelem = document.createElement("p");
-    bodyelem.innerText = body;
-    obal.appendChild(bodyelem);
-    outputPlace.appendChild(obal);
+  postBlog(header, body, numbr) {
+    //console.log("postblog num is " + numbr)
+    if (numbr < 0 || numbr == null) {
+      let outputPlace = document.getElementById("BlogPostY");
+      let obal = document.createElement("div");
+      let headrelem = document.createElement("h3");
+      headrelem.innerText = header;
+      obal.appendChild(headrelem);
+      let bodyelem = document.createElement("p");
+      bodyelem.innerText = body;
+      obal.appendChild(bodyelem);
+      outputPlace.appendChild(obal);
+    } else {
+      if (header == "" && body == "") {
+        document.getElementById("BlogPostY").removeChild(numbr);
+      } else {
+        let outputPlace = document.getElementById("BlogPostY").childNodes[numbr];
+        outputPlace.childNodes[0].textContent = header;
+        outputPlace.childNodes[1].textContent = body;
+      }
+    }
+
   }
 }
