53e9132d588e6f18ce4518461f12a1494d413e6c
Chesters-CZ <bezouskavit@gmail.com>
2021-01-04 16:52:28 +0100

    nejhežčí pajpa (wanlainer)

--- apliklace/src/app/excrept.pipe.ts
+++ apliklace/src/app/excrept.pipe.ts
@@ -1,14 +1,12 @@
 import {Pipe, PipeTransform} from '@angular/core';
+import {split} from "ts-node";
 
 @Pipe({
   name: 'excrept'
 })
 export class ExcreptPipe implements PipeTransform {
 
-  transform(value: string, times?: number): string {
-    let out = "";
-    if (times == null) times = 5;
-    for (let i = 0; times < i && times < value.length; times++) out = out + value.split("")[i];
-    return out + "...";
+  transform(value: string, times: number = 5, ending: string = "..."): string {
+    return value.substring(0, times) + ending;
   }
 }
