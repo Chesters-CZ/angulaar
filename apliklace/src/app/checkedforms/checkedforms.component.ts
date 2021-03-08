45f6bb6a10fc93e2fb7ccf120c877f823bef5489
Chesters-CZ <bezouskavit@gmail.com>
2021-03-08 14:48:03 +0100

    desktop inin't

--- apliklace/src/app/checkedforms/checkedforms.component.ts
+++ apliklace/src/app/checkedforms/checkedforms.component.ts
@@ -2,7 +2,6 @@ import {Component, Injectable, OnInit} from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import {HttpClientModule, HttpHeaders} from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
-import * as  http from 'http';
 
 @Injectable({
   providedIn: 'root'
@@ -24,11 +23,9 @@ export class CheckedformsComponent implements OnInit {
 
   addUnameToUnames() {
     this.usernames[this.usernames.length] = this.username;
-    http.put("https://webhook.site/c69b9ba0-5ca3-44c9-9ba1-737071941d61", this.username)
+    //HttpClient.put("https://webhook.site/c69b9ba0-5ca3-44c9-9ba1-737071941d61", this.username)
     this.username = null;
   }
-
-  debugstuff
 
   constructor() { }
 
